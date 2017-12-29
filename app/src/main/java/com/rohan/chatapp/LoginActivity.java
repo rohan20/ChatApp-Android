4ac35f528eb680f74f48ea9a03238424fb19297d
Rohan Taneja <rohan.bvcoe@gmail.com>
2017-12-29 12:16:15 +0530

    configured databinding

--- app/src/main/java/com/rohan/chatapp/LoginActivity.java
+++ app/src/main/java/com/rohan/chatapp/LoginActivity.java
@@ -1,13 +1,18 @@
 package com.rohan.chatapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.rohan.chatapp.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
     }
 }
