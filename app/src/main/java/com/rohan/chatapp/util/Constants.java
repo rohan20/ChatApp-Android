8794d5b684ed82ec4075b2acaa835606a071e74c
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-01-01 23:52:22 +0530

    added constants for chat

---
+++ app/src/main/java/com/rohan/chatapp/util/Constants.java
@@ -0,0 +1,13 @@
+package com.rohan.chatapp.util;
+
+/**
+ * Created by rohan on 01/01/18.
+ */
+
+public class Constants {
+
+    public static final String HOSTNAME = "";
+    public static final int PORT = 5222;
+    public static final String SERVICE_NAME = "";
+
+}
